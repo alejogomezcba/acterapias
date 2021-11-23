f76cb43c0ef5e71efdea6306a3cd321ef9b8b763
Alejo Gomez <alejorgomez@hotmail.com>
2021-11-23 17:13:55 -0300

    update files

--- contacto.php
+++ contacto.php
@@ -5,12 +5,12 @@ $telefono = $_POST['telefono'];
 $email = $_POST['email'];
 $mensaje = $_POST['mensaje'];
 $para = 'anadcenturion@gmail.com';
-$asunto = 'Consulta de página';
+$asunto = 'Consulta desde AC Terapias';
 $header = 'De: ' . $email;
-$msjCorreo = "Nombre: $nombre\n Apellido: $apellido\n Teléfono: $telefono\n E-Mail: $email\n Mensaje:\n $mensaje";
+$msjCorreo =" Nombre: $nombre\n Apellido: $apellido\n Teléfono: $telefono\n E-Mail: $email\n Mensaje:\n $mensaje";
   
 if ($_POST['submit']) {
-if (mail($para, $titulo, $msjCorreo, $header)) {
+if (mail($para, $asunto, $msjCorreo, $header)) {
 echo "<script language='javascript'>
 alert('Mensaje enviado, muchas gracias.');
 window.location.href = 'http://acterapias.com';
